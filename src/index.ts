a09b221b869338f552b1a840c54d167a4d5c55d9
MiriamQi <miriamqi@amazon.com>
2020-05-31 19:47:23 -0700

    basic syntax

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 import express from 'express';
 const app = express();
+
+//use type for non object
+//function
+type Fn = () => void;
+type Add = (x: Parms) => number;
+
+//use interface for object
+interface Parms {
+    a: number;
+    b: number;
+}
+
+const add = (a: number, b: number):number => {
+    //@ts-ignore: if undefined: use !, or if option
+    return a + b;
+}
+
+app.get('/', (req: any) => {
+    req.name = 'bob'
+    console.log(req.name)
+    add(1,2)
+})
+app.listen(3001, ()=>{
+    console.log("started");
+});
